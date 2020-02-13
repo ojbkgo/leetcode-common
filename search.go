b2670ef00dfb915da7bc9cddc3da593874cf4b46
wenziyue <wenzy@weipaitang.com>
2020-02-14 01:12:54 +0800

    二分查找

--- search.go
+++ search.go
@@ -1,31 +1,114 @@
 package common
 
-func BinarySearchInt(data []int, target int) (int, bool) {
 
-	if len(data) == 0 {
+//查找第一个等于给定值的元素
+func BinarySearchFirst(a []int, v int) int {
+	n := len(a)
+	if n == 0 {
+		return -1
+	}
+
+	low := 0
+	high := n - 1
+	for low <= high {
+		mid := (low + high) >> 1
+		if a[mid] > v {
+			high = mid - 1
+		} else if a[mid] < v {
+			low = mid + 1
+		} else {
+			if mid == 0 || a[mid-1] != v {
+				return mid
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
+
+//查找最后一个值等于给定值的元素
+func BinarySearchLast(a []int, v int) int {
+	n := len(a)
+	if n == 0 {
+		return -1
+	}
+
+	low := 0
+	high := n - 1
+	for low <= high {
+		mid := (low + high) >> 1
+		if a[mid] > v {
+			high = mid - 1
+		} else if a[mid] < v {
+			low = mid + 1
+		} else {
+			if mid == n-1 || a[mid+1] != v {
+				return mid
+			} else {
+				low = mid + 1
+			}
+		}
+	}
+
+	return -1
+}
+
+//查找第一个大于等于给定值的元素
+func BinarySearchFirstGT(a []int, v int) (int, bool) {
+	n := len(a)
+	if n == 0 {
 		return -1, false
 	}
 
-	var l, r, m int
-
-	l = 0
-	r = len(data) - 1
-	m = (l + r) / 2
-
-	for l < r {
-		if target > data[m] {
-			l = m+1
-		} else if target < data[m] {
-			r = m
+	low := 0
+	high := n - 1
+	for low <= high {
+		//避免溢出
+		mid := low + (high-low)>>1
+		if a[mid] >= v {
+			if mid == 0 || a[mid-1] < v {
+				if a[mid] == v {
+					return mid, true
+				}
+				return mid, false
+			} else {
+				high = mid - 1
+			}
 		} else {
-			return m, true
+			low = mid + 1
 		}
-		m = (l + r) / 2
 	}
 
-	return l, false
+	return -1, false
 }
 
-func BinarySearchString(data []int, target int) (int, bool) {
+//查找最后一个小于等于给定值的元素
+func BinarySearchLastLT(a []int, v int) (int, bool) {
+	n := len(a)
+	if n == 0 {
+		return -1, false
+	}
+
+	low := 0
+	high := n - 1
+	for low <= high {
+		mid := low + (high-low)>>1
+		if a[mid] > v {
+			high = mid - 1
+		} else {
+			if mid == n-1 || a[mid+1] > v {
+				if a[mid] == v {
+					return mid, true
+				}
+
+				return mid, false
+			} else {
+				low = mid + 1
+			}
+		}
+	}
+
 	return -1, false
 }
--- search_test.go
+++ search_test.go
@@ -6,17 +6,19 @@ import (
 )
 
 func Test_BinarySearchInt(t *testing.T) {
-	fmt.Println(BinarySearchInt([]int{1, 2, 3}, 2))
-	fmt.Println(BinarySearchInt([]int{1, 2, 4}, 3))
-	fmt.Println(BinarySearchInt([]int{1, 3, 4}, 2))
-	fmt.Println(BinarySearchInt([]int{1, 3, 4}, 1))
-	fmt.Println(BinarySearchInt([]int{1, 3, 4}, 4))
-	fmt.Println(BinarySearchInt([]int{1, 3, 4}, 100))
-	fmt.Println(BinarySearchInt([]int{1, 3, 4}, 0))
+	fmt.Println(BinarySearchFirstGT([]int{1, 2, 3}, 2))
+	fmt.Println(BinarySearchFirstGT([]int{1, 2, 4}, 3))
+	fmt.Println(BinarySearchFirstGT([]int{1, 3, 4}, 2))
 
-	fmt.Println(BinarySearchInt([]int{1, 2, 3, 5}, 2))
-	fmt.Println(BinarySearchInt([]int{1, 2, 4, 6}, 3))
-	fmt.Println(BinarySearchInt([]int{1, 3, 4, 6}, 2))
-	fmt.Println(BinarySearchInt([]int{1, 3, 4, 6}, 1))
-	fmt.Println(BinarySearchInt([]int{1, 3, 4, 6}, 4))
+	fmt.Println(BinarySearchFirstGT([]int{1, 3, 4}, 1))
+	fmt.Println(BinarySearchFirstGT([]int{1, 3, 4}, 4))
+
+	fmt.Println(BinarySearchFirstGT([]int{1, 3, 4}, 100))
+	fmt.Println(BinarySearchFirstGT([]int{1, 3, 4}, 0))
+
+	fmt.Println(BinarySearchFirstGT([]int{1, 2, 3, 5}, 2))
+	fmt.Println(BinarySearchFirstGT([]int{1, 2, 4, 6}, 3))
+	fmt.Println(BinarySearchFirstGT([]int{1, 3, 4, 6}, 2))
+	fmt.Println(BinarySearchFirstGT([]int{1, 3, 4, 6}, 1))
+	fmt.Println(BinarySearchFirstGT([]int{1, 3, 4, 6}, 4))
 }
