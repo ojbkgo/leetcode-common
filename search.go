ed2e8b14ef08bb38f1535451432c5fe282fbfee5
wenziyue <wenzy@weipaitang.com>
2020-02-13 18:52:13 +0800

    ok

--- search.go
+++ search.go
@@ -1,13 +1,29 @@
 package common
 
-
 func BinarySearchInt(data []int, target int) (int, bool) {
 
 	if len(data) == 0 {
 		return -1, false
 	}
 
+	var l, r, m int
 
+	l = 0
+	r = len(data) - 1
+	m = (l + r) / 2
+
+	for l < r {
+		if target > data[m] {
+			l = m+1
+		} else if target < data[m] {
+			r = m
+		} else {
+			return m, true
+		}
+		m = (l + r) / 2
+	}
+
+	return l, false
 }
 
 func BinarySearchString(data []int, target int) (int, bool) {
---
+++ search_test.go
@@ -0,0 +1,22 @@
+package common
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_BinarySearchInt(t *testing.T) {
+	fmt.Println(BinarySearchInt([]int{1, 2, 3}, 2))
+	fmt.Println(BinarySearchInt([]int{1, 2, 4}, 3))
+	fmt.Println(BinarySearchInt([]int{1, 3, 4}, 2))
+	fmt.Println(BinarySearchInt([]int{1, 3, 4}, 1))
+	fmt.Println(BinarySearchInt([]int{1, 3, 4}, 4))
+	fmt.Println(BinarySearchInt([]int{1, 3, 4}, 100))
+	fmt.Println(BinarySearchInt([]int{1, 3, 4}, 0))
+
+	fmt.Println(BinarySearchInt([]int{1, 2, 3, 5}, 2))
+	fmt.Println(BinarySearchInt([]int{1, 2, 4, 6}, 3))
+	fmt.Println(BinarySearchInt([]int{1, 3, 4, 6}, 2))
+	fmt.Println(BinarySearchInt([]int{1, 3, 4, 6}, 1))
+	fmt.Println(BinarySearchInt([]int{1, 3, 4, 6}, 4))
+}
