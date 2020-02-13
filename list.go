7ab3a582dfc8cbe7f52869552413659e5919affc
wenziyue <wenzy@weipaitang.com>
2020-02-14 01:55:30 +0800

    array to list

--- list.go
+++ list.go
@@ -1,1 +1,53 @@
 package common
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func Array2List(data []int) *ListNode {
+	var head *ListNode
+	var tail *ListNode
+
+	if len(data) == 0 {
+		return nil
+	}
+
+	head = &ListNode{
+		Val: data[0],
+	}
+
+	if len(data) == 1 {
+		return head
+	}
+
+	tail = head
+
+	for _, v := range data[1:] {
+		node := &ListNode{
+			Val:v,
+		}
+
+		tail.Next = node
+		tail = node
+	}
+
+	return head
+}
+
+func TravelList(list *ListNode, action func(v int)) {
+	for list != nil {
+		action(list.Val)
+		list = list.Next
+	}
+}
+
+func DumpList(list *ListNode) {
+	data := make([]int, 0)
+
+	TravelList(list, func(v int) {
+		data = append(data, v)
+	})
+
+	Dump(data)
+}
--- list_test.go
+++ list_test.go
@@ -1,1 +1,14 @@
 package common
+
+import "testing"
+
+func Test_Array2List(t *testing.T) {
+	data := []int{1, 2, 3, 4, 5}
+	DumpList(Array2List(data))
+	data = []int{1, 2}
+	DumpList(Array2List(data))
+	data = []int{1}
+	DumpList(Array2List(data))
+	data = []int{}
+	DumpList(Array2List(data))
+}
